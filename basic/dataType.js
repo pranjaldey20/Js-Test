f83133a1c9d5fae870b292078300eeb2048853a5
Pranjal Dey <59050963+pranjaldey20@users.noreply.github.com>
2025-11-12 08:17:13 +0000

    datatype

---
+++ basic/dataType.js
@@ -0,0 +1,25 @@
+"use strict" 
+// alert(3+3) // we are using nodejs, not browser
+
+console.log (3+6)
+
+console.log("Pranjal Dey")
+
+let name = "Pranjal Dey"
+let age = 24
+let isLoggedIn = false
+
+// type variable
+//number 
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol
+
+//object
+
+
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 454521
+let accountEmail = "abc123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata" 
+
+//accountId = 5
+accountEmail = "abhs@gmail.com" 
+accountPassword = "25856"
+accountCity = "kolkata"
+
+console.log(accountId)
+
+
+console. table ([accountId,accountEmail,accountPassword,accountCity])
