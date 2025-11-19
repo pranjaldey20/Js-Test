cd33935d2dce3eb90a76f19e6259988f24b539ad
Pranjal Dey <59050963+pranjaldey20@users.noreply.github.com>
2025-11-19 16:51:57 +0000

    coversional operation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "Prnajal"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33= 33 number
+//"33abc" = string
+//null = 0 
+//undefined = no define
+//boolean = true and false
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
